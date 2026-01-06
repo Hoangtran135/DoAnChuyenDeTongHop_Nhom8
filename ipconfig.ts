c8c361466be8bf81a2f57b57e4e3cf77cb8cebef
KHEANVAKHIM <vakhimkhean@gmail.com>
2026-01-07 00:05:44 +0700

    Backend config and database setup

--- ipconfig.ts
+++ ipconfig.ts
@@ -1,6 +1,6 @@
 import { Platform } from "react-native";
 const SERVER_PORT = 3000;
-const SERVER_IP = "192.168.1.16";
+const SERVER_IP = "192.168.1.139";
 export const BASE_URL = `http://${SERVER_IP}:${SERVER_PORT}`;
 export const initializeIP = async (): Promise<void> => {};
 export const setServerIP = async (_ip: string): Promise<void> => {};
